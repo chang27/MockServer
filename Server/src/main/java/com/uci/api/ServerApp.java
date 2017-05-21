a8ff8d3b3dd01c41e284092a30ae5d7fc0c9e57a
majun <zte_majun@126.com>
2017-05-20 17:12:05 -0700

    add asy query

--- Server/src/main/java/com/uci/api/ServerApp.java
+++ Server/src/main/java/com/uci/api/ServerApp.java
@@ -1,9 +1,14 @@
 package com.uci.api;
 
+import com.uci.mode.Payload;
 import com.uci.mode.Response;
+import com.uci.mode.SensorData;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.sql.Timestamp;
 
 /**
  * Created by junm5 on 5/1/17.
@@ -11,22 +16,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Component
 public class ServerApp {
 
-    private String DEFAULT_REPLY = "OK";
+    private String DEFAULT_REPLY = "Test Data";
 
-    @RequestMapping(path = "/get", method = RequestMethod.GET)
+
+    @RequestMapping(path = "/query", method = RequestMethod.GET)
     public Response get() {
         return Response.success(DEFAULT_REPLY);
     }
 
-
     @RequestMapping(path = "/post", method = RequestMethod.POST)
-    public Response post() {
-        return Response.success("OK");
+    public Response post(@RequestBody Integer id) {
+        return Response.success(new SensorData()
+                .setId(id)
+                .setObservation_type_id(1)
+                .setPayload(new Payload().setClint_id("d6767f4b93cc35ae5f339e5ce5fa268ea11614ce"))
+                .setSensor_id("3143-clwa-3039")
+                .setTimestamp(Timestamp.valueOf("2017-04-03 12:53:57")));
     }
 
+    /**
+     * @param id
+     * @return
+     */
+    @RequestMapping(path = "/post", method = RequestMethod.POST)
+    public Response asyTask(@RequestBody Integer id) {
+        Response ok = Response.success("OK");
 
-    @RequestMapping(path = "/ping", method = RequestMethod.GET)
-    public Response ping() {
-        return Response.success(DEFAULT_REPLY);
+        return ok;
     }
 }
