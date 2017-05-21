d9cc578c83a0f2d17dc61dc61b9c54a8c652c98a
majun <zte_majun@126.com>
2017-05-20 17:13:50 -0700

    asy task

--- Server/src/main/java/com/uci/api/ServerApp.java
+++ Server/src/main/java/com/uci/api/ServerApp.java
@@ -4,6 +4,7 @@ import com.uci.mode.Payload;
 import com.uci.mode.Response;
 import com.uci.mode.SensorData;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,16 +17,8 @@ import java.sql.Timestamp;
 @Component
 public class ServerApp {
 
-    private String DEFAULT_REPLY = "Test Data";
-
-
-    @RequestMapping(path = "/query", method = RequestMethod.GET)
-    public Response get() {
-        return Response.success(DEFAULT_REPLY);
-    }
-
-    @RequestMapping(path = "/post", method = RequestMethod.POST)
-    public Response post(@RequestBody Integer id) {
+    @RequestMapping(path = "/query/{id}", method = RequestMethod.GET)
+    public Response get(@PathVariable Integer id) {
         return Response.success(new SensorData()
                 .setId(id)
                 .setObservation_type_id(1)
@@ -34,14 +27,11 @@ public class ServerApp {
                 .setTimestamp(Timestamp.valueOf("2017-04-03 12:53:57")));
     }
 
-    /**
-     * @param id
-     * @return
-     */
     @RequestMapping(path = "/post", method = RequestMethod.POST)
-    public Response asyTask(@RequestBody Integer id) {
+    public Response post(@RequestBody Integer id) {
         Response ok = Response.success("OK");
 
         return ok;
     }
+
 }
